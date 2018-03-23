02b9e046174193b63183744693bee6d32bd54e23
WangB <wxjwb@qq.com>
2018-03-23 11:25:20 +0800

    add junit pom

--- src/main/java/com/caipiao/zuul/CpZuulApplication.java
+++ src/main/java/com/caipiao/zuul/CpZuulApplication.java
@@ -1,19 +1,11 @@
 package com.caipiao.zuul;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.ZuulServerMarkerConfiguration;
-import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import com.caipiao.zuul.filter.LoginCheckFilter;
 
